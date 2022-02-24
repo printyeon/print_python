0abd453a98e76e98f680bd229c7f9a6f8a35f15e
printyeon <96305365+printyeon@users.noreply.github.com>
2022-02-24 21:56:52 +0900

    turtle ht
    거북이로 하트 만들기

---
+++ turtle_ht.py
@@ -0,0 +1,42 @@
+import turtle
+t=turtle.Pen()
+t.pencolor("blue")
+t.shape("turtle")
+t.pensize(10)
+t.speed("fast")
+
+##for i in range(100) :
+##    t.forward(1)
+##    t.right(1)
+##t.right(40)
+##t.forward(100)
+##t.right(0)
+##t.forward(100)
+
+##for i in range(5) :
+##    t.forward(200)
+##    t.right(72)
+t.right(300)
+for i in range(30) :
+    t.forward(5)
+    t.right(5)
+t.forward(40)
+t.right(10)
+for i in range(5) :
+    t.forward(5)
+    t.right(5)
+##t.forward(5)
+t.right(10)
+t.forward(150)
+t.right(90)
+t.forward(130)
+for i in range(5) :
+    t.forward(5)
+    t.right(5)
+t.right(10)
+t.forward(40)
+for i in range(33) :
+    t.forward(5)
+    t.right(5)
+t.right(300)
+t.forward(5)
